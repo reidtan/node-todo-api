82a97e46af5d7257c3ce8056f027d824577c5918
Reid Tan <huazhant@ncs.com.sg>
2017-05-25 14:53:09 +0800

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -24,6 +25,25 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  let {id} = req.params;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      if(!todo) {
+        return res.status(404).send({});
+      }
+      res.send({todo});
+    })
+    .catch((err) => {
+      res.status(400).send(err.message);
+    });
 });
 
 app.listen(3000, () => {
