eb61c5343690f4e7a0a512f91e1a3326bf0bf6f2
Reid Tan <huazhant@ncs.com.sg>
2017-05-26 13:57:28 +0800

    Updated MONGO_URI for prod env

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if(env === 'development') {
 } else if(env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+  process.env.MONGO_URI = 'mongodb://todoapp:X6hD30mCv8Cl@ds151461.mlab.com:51461/todoapp';
 }
