c2bd09554e9ef858c5c40ab0bf6454208379f8ba
Reid Tan <huazhant@ncs.com.sg>
2017-05-26 13:54:23 +0800

    Setup seperate test and dev env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 let mongoose = require('mongoose');
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://todoapp:X6hD30mCv8Cl@ds151461.mlab.com:51461/todoapp',
-};
-
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect(process.env.MONGO_URI);
 
 module.exports = {mongoose};
