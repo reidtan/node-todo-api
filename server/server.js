67c30222ec669a4f72ea83de4df23bfe72ee1440
Reid Tan <huazhant@ncs.com.sg>
2017-05-26 10:06:24 +0800

    Test error message

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -63,6 +64,32 @@ app.delete('/todos/:id', (req, res) => {
     })
     .catch((err) => {
       res.status(400).send(err.message);
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let {id} = req.params;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then((todo) => {
+      if(!todo) {
+        res.status(404);
+      }
+      res.send({todo});
+    }).catch((err) => {
+      res.status(400).send();
     });
 });
 
